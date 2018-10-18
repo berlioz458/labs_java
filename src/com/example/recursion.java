46fcfda7ae6451e81c0fbea42a2fd2b50bc722af
shulinina.e <test-2@prodv.net>
2018-10-18 09:03:07 +0700

    реализован рекурсивный факториал

--- src/com/example/recursion.java
+++ src/com/example/recursion.java
@@ -1,4 +1,22 @@
 package com.example;
 
 public class recursion {
+
+    public static int factorial(int x)
+    {
+        if (x == 1)
+        {
+            return 1;
+        }else
+        {
+            return x * factorial(x - 1);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int X;
+        X = factorial(11);
+        System.out.println(X);
+    }
 }
