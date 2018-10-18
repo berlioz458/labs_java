46d1c9897a099f54ad983499441e132d1bc11e90
shulinina.e <test-2@prodv.net>
2018-10-18 12:09:53 +0700

    summa chisel

--- src/com/example/recursion.java
+++ src/com/example/recursion.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import java.util.ArrayList;
+
 public class recursion {
 
     public static int factorial(int x)
@@ -13,10 +15,36 @@ public class recursion {
         }
     }
 
+    public static int summa_array (ArrayList array)
+    {
+        int summ;
+        if (array.size() == 0)
+        {
+            summ = 0;
+            return summ;
+        }else
+        {
+            summ= (int) array.get(0);
+            array.remove(0);
+            return summ + summa_array(array);
+        }
+
+        //return (int) summa_array(array[array.length-1]);
+    }
 
     public static void main(String[] args) {
         int X;
         X = factorial(11);
-        System.out.println(X);
+        //System.out.println(X);
+        ArrayList A = new ArrayList();
+        A.add(1);
+        A.add(6);
+        A.add(3);
+        A.add(9);
+        A.add(8);
+        int SUMMA = summa_array(A);
+        System.out.println(SUMMA);
     }
+
+
 }
