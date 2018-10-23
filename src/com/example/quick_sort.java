f254a3d973fa5125d59a393cbe10902e1d85a8b0
shulinina.e <test-2@prodv.net>
2018-10-23 16:30:15 +0700

    сделала вынуждено и криво, рандомный элемент. через раз выдается ошибка.

--- src/com/example/quick_sort.java
+++ src/com/example/quick_sort.java
@@ -4,32 +4,38 @@ import java.util.ArrayList;
 
 public class quick_sort {
 
-
     public static ArrayList quick_sorting(ArrayList A)
     {
+
         if (A.size() < 2)
         {
             return A;
+
         }
         else
         {
-            int pivot = (int) A.get(0);
+            //int pivot = (int) A.get(0);
+            int pivot = (int) Math.round((Math.random() * A.size()));
             ArrayList less = new ArrayList();
             ArrayList greate = new ArrayList();
-            for (int i = 1; i < A.size(); i++)
+            for (int i = 0; i < A.size(); i++)
             {
-                if ((int)A.get(i) < pivot)
+                if ((int)A.get(i) < (int)A.get(pivot))
                 {
                     less.add(A.get(i));
                 }
-                if ((int)A.get(i) > pivot)
+                if ((int)A.get(i) > (int)A.get(pivot))
                 {
                     greate.add(A.get(i));
+                }
+                if ((int)A.get(i) == (int)A.get(pivot))
+                {
+                    //break;
                 }
             }
             ArrayList Result = new ArrayList();
             Result.add(quick_sorting(less));
-            Result.add(pivot);
+            Result.add(A.get(pivot));
             Result.add(quick_sorting(greate));
 
 
@@ -39,7 +45,7 @@ public class quick_sort {
     }
 
     public static void main(String[] args) {
-        ArrayList AAA = new ArrayList();;
+        ArrayList AAA = new ArrayList();
         AAA.add(2);
         AAA.add(1);
         AAA.add(14);
