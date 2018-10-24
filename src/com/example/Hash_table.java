96ab770e108897b847618b3f32f9c09ffe4918af
shulinina.e <test-2@prodv.net>
2018-10-24 10:01:01 +0700

    add new for 5.6 and 5.7

--- src/com/example/Hash_table.java
+++ src/com/example/Hash_table.java
@@ -1,26 +1,58 @@
 package com.example;
 
 import java.util.HashMap;
-import java.util.Scanner;
 
 public class Hash_table {
+
+    public static void batery()
+    {
+        HashMap<String,Double> Batery = new HashMap<>();
+        Batery.put("AA", 1.5);
+        Batery.put("A", 12.0);
+        Batery.put("AAA", 1.2);
+        Batery.put("AAAA", 1.5);
+
+        System.out.println(Batery);
+        System.out.println(Batery.get("AAA"));
+    }
+    
+    public static void book()
+    {
+        HashMap<String,String> Book = new HashMap<>();
+        Book.put("маус", "Арт Шпигельман");
+        Book.put("Fun Home", "Элисон Бекдел");
+        Book.put("Хранители", "Alan Moore");
+
+        System.out.println(Book);
+        System.out.println(Book.get("Хранители"));
+    }
+
+    public static void print_hash(HashMap h1)
+    {
+        for(int k = 0; k < h1.size(); k++)
+        {
+            System.out.println(k + ": " + h1.get(k));
+        }
+    }
+
     public static void main(String[] args) {
-        HashMap<String, String> Call_book= new HashMap<>();
+        /*HashMap<Integer, String> Call_book= new HashMap<>();
         Scanner in = new Scanner(System.in);
         for (int i = 0; i < 10; i++)
         {
-            Call_book.put(in.next(),in.next());
+            Call_book.put(in.nextInt(),in.next());
         }
-
         System.out.println(Call_book);
-
-        String index = "vxod"; // надо придумать нормальный выход из цикла while
+        print_hash(Call_book); //криво вынесено в отдельную функцию вывода, чтобы с каждой строки была новая пара
+        /*String index = "vxod"; // надо придумать нормальный выход из цикла while
         while (!index.equals("exit"))
         {
             index = in.next();
             System.out.println(Call_book.get(index));
-        }
+        }*/
 
+        //batery();
+        book();
     }
 }
 
@@ -32,3 +64,9 @@ public class Hash_table {
 //HashMap - это реализованные уже в Java хеш-таблицы
 //containsKey - поиск по ключу (true/false)
 //containsValue - поиск по значению (true/false)
+
+
+//10 элементов : 1 - плохое распределение, 2 - плохое, 3 - плохое/среднее, 4 - нормальное
+//5.5. уже сделано выше
+//5.6. batery()
+//5.7. book()
